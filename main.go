c01874c8be5bf63776468cbb0249082e259e9887
Haroon Albar <haroonalbarx@gmail.com>
2024-05-06 06:15:13 +0530

    diffent method

--- main.go
+++ main.go
@@ -5,19 +5,19 @@ import (
 	"time"
 )
 
-func say (s string) {
-  for i:=0 ; i < 5 ; i++{
-    time.Sleep(10*time.Millisecond)
-    fmt.Printf("%v: %v\n",s,i)
-  }
+func say(s string, i int) {
+	fmt.Printf("%v: %v\n", s, i)
 }
 
 func main() {
-  go say("Hello")
-  go say("Boi")
-  time.Sleep(30*time.Millisecond)
+	for i := 0; i < 5; i++ {
+		go say("Hello", i)
+		go say("Boi", i)
+	}
+	time.Sleep(1 * time.Millisecond)
 }
-// here im seeing a pattern where all the go routines are not executed on 30 ms 
-// execution pause there both gorutines are run but the result im getting is only upto i=2 
+
+// here im seeing a pattern where all the go routines are not executed on 30 ms
+// execution pause there both gorutines are run but the result im getting is only upto i=2
 // and if I give more execution pause more of the function is executed.
 // i'll look further into it later
